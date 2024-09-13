02dc26268275dc3defa8aea4f32ee90c6e306500
LanHuong1507 <taejoon1507@gmail.com>
2024-09-13 10:07:24 +0700

    create project and navbar menu in web and responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('mobile-menu-btn').addEventListener('click', function () {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('active');
+});
+
+document.getElementById('mobile-menu-close').addEventListener('click', function () {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.remove('active');
+});
